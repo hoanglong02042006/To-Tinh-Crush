1fdf439d4e29e5e466ace0632f7fa6ee9dc7bbf0
hoanglong02042006 <86709143+hoanglong02042006@users.noreply.github.com>
2021-06-30 22:10:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ const CONFIG = {
     reply: 'Hong biếttt',
     mess: 'iu chịiii',
     messDesc: 'Chơi game hongg',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lunn <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
